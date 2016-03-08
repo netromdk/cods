49af8a00d34c1172ea4c0ba0a592b049fe8e791d
Morten Kristensen <msk@nullpointer.dk>
2016-03-08 19:31:57 +0100

    Vector: Split template impl. into .hpp file.

--- cods/Vector.h
+++ cods/Vector.h
@@ -14,177 +14,62 @@ template <typename T,        ///< Item type.
 class Vector {
 public:
   /// Create empty vector with no capacity.
-  Vector() : items(0), cap(0), data(nullptr) { }
+  Vector();
 
   /// Create vector filled with \p size elements of value \p val.
-  Vector(int size, T val) : Vector() {
-    alloc(size);
-    for (decltype(size) i = 0; i < size; i++) {
-      // Don't check capacity because we just allocated all. Will speed up a lot.
-      _append(val, false);
-    }
-  }
+  Vector(int size, T val);
 
   /// Create vector from the values of raw array \p values.
-  Vector(int size, T *values) : Vector() {
-    alloc(size);
-    for (decltype(size) i = 0; i < size; i++) {
-      _append(values[i], false);
-    }
-  }
+  Vector(int size, T *values);
 
   /// Create vector from iterator range.
   template <typename Iterator>
-  Vector(Iterator first, Iterator last) : Vector() {
-    for (; first != last; first++) {
-      append(*first);
-    }
-  }
+  Vector(Iterator first, Iterator last);
 
-  ~Vector() {
-    clear();
-  }
+  ~Vector();
 
-  bool isEmpty() const {
-    return items == 0;
-  }
-
-  int size() const {
-    return items;
-  }
-
-  int capacity() const {
-    return cap;
-  }
+  bool isEmpty() const;
+  int size() const;
+  int capacity() const;
 
   /// Insert value at the end.
-  void append(const T &val) {
-    _append(val);
-  }
+  void append(const T &val);
 
   /// Insert value at the beginning.
-  void prepend(const T &val) {
-    insert(0, val);
-  }
+  void prepend(const T &val);
 
-  void insert(int pos, const T &val) {
-    assert(pos >= 0 && pos < cap && "Position out of bounds!");
-    checkAlloc();
-    shiftLeft(pos); // Effectively moving right.
-    data[pos] = val;
-    items++;
-  }
+  void insert(int pos, const T &val);
 
-  void print() const {
-    cout << "[ ";
-    if (isEmpty()) {
-      cout << "empty";
-    }
-    for (decltype(cap) i = 0; i < cap; i++) {
-      cout << convert(data[i]);
-      if (i < cap - 1) {
-        cout << ", ";
-      }
-    }
-    cout << " ]" << endl;
-  }
+  void print() const;
 
-  void clear() {
-    if (data) {
-      delete[] data;
-      data = nullptr;
-    }
-    items = cap = 0;
-  }
+  void clear();
 
-  void shrinkToFit() {
-    if (items == cap) return;
-    alloc(items);
-  }
+  void shrinkToFit();
 
-  T operator[](int pos) {
-    assert(pos >= 0 && pos < cap && "Position out of bounds!");
-    return data[pos];
-  }
+  T operator[](int pos);
+  const T &operator[](int pos) const;
 
-  const T &operator[](int pos) const {
-    assert(pos >= 0 && pos < cap && "Position out of bounds!");
-    return data[pos];
-  }
-
-  void remove(const T &val) {
-    removeFrom(val);
-  }
-
-  void removeAt(int pos) {
-    assert(pos >= 0 && pos < cap && "Position out of bounds!");
-    data[pos] = T();
-    items--;
-    shiftRight(pos); // Effectively moving left.
-  }
+  void remove(const T &val);
+  void removeAt(int pos);
 
 private:
-  void fillDefault(T *ptr, int n) {
-    for (decltype(n) i = 0; i < n; i++) {
-      ptr[i] = T();
-    }
-  }
-
-  void alloc(int size) {
-    auto newCap = size;
-    auto *newData = new T[newCap];
-    if (newCap > cap) {
-      fillDefault(newData, newCap);
-    }
-    if (items > 0) {
-      memcpy(newData, data, items * sizeof(T));
-      delete[] data;
-    }
-    data = newData;
-    cap = newCap;
-  }
-
-  void checkAlloc() {
-    if (items != cap) return;
-    alloc(cap == 0 ? INIT_CAP : cap * CAP_MULT);
-  }
+  void fillDefault(T *ptr, int n);
+  void alloc(int size);
+  void checkAlloc();
 
   /// Appends value but only checks capacity if check is set.
   /** This saves processing time when appending very many items. */
-  void _append(const T &val, bool check = true) {
-    if (check) {
-      checkAlloc();
-    }
-    data[items++] = val;
-  }
+  void _append(const T &val, bool check = true);
 
-  void shiftRight(int pos) {
-    for (decltype(pos) i = pos; i < items; i++) {
-      std::swap(data[i], data[i+1]);
-    }
-  }
+  void shiftRight(int pos);
+  void shiftLeft(int pos);
 
-  void shiftLeft(int pos) {
-    for (decltype(pos) i = items; i >= pos; i--) {
-      std::swap(data[i], data[i+1]);
-    }
-  }
-
-  void removeFrom(const T &val, int pos = 0) {
-    for (decltype(pos) i = pos; i < items; i++) {
-      auto &item = data[i];
-      if (item == val) {
-        item = T(); // Clear.
-        items--;
-        shiftRight(i); // Effectively moving left.
-        removeFrom(val, i);
-        break;
-      }
-    }
-  }
+  void removeFrom(const T &val, int pos = 0);
 
   int items, cap;
   T *data;
 };
+
+#include "cods/Vector.hpp"
 
 CODS_END_NAMESPACE
---
+++ cods/Vector.hpp
@@ -0,0 +1,189 @@
+template <typename T, int INIT_CAP, int CAP_MULT>
+Vector<T, INIT_CAP, CAP_MULT>::Vector() : items(0), cap(0), data(nullptr) { }
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+Vector<T, INIT_CAP, CAP_MULT>::Vector(int size, T val) : Vector() {
+  alloc(size);
+  for (decltype(size) i = 0; i < size; i++) {
+    // Don't check capacity because we just allocated all. Will speed up a lot.
+    _append(val, false);
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+Vector<T, INIT_CAP, CAP_MULT>::Vector(int size, T *values) : Vector() {
+  alloc(size);
+  for (decltype(size) i = 0; i < size; i++) {
+    _append(values[i], false);
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+template <typename Iterator>
+Vector<T, INIT_CAP, CAP_MULT>::Vector(Iterator first, Iterator last) : Vector() {
+  for (; first != last; first++) {
+    append(*first);
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+Vector<T, INIT_CAP, CAP_MULT>::~Vector() {
+  clear();
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+bool Vector<T, INIT_CAP, CAP_MULT>::isEmpty() const {
+  return items == 0;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+int Vector<T, INIT_CAP, CAP_MULT>::size() const {
+  return items;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+int Vector<T, INIT_CAP, CAP_MULT>::capacity() const {
+  return cap;
+}
+
+/// Insert value at the end.
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::append(const T &val) {
+  _append(val);
+}
+
+/// Insert value at the beginning.
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::prepend(const T &val) {
+  insert(0, val);
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::insert(int pos, const T &val) {
+  assert(pos >= 0 && pos < cap && "Position out of bounds!");
+  checkAlloc();
+  shiftLeft(pos); // Effectively moving right.
+  data[pos] = val;
+  items++;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::print() const {
+  cout << "[ ";
+  if (isEmpty()) {
+    cout << "empty";
+  }
+  for (decltype(cap) i = 0; i < cap; i++) {
+    cout << convert(data[i]);
+    if (i < cap - 1) {
+      cout << ", ";
+    }
+  }
+  cout << " ]" << endl;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::clear() {
+  if (data) {
+    delete[] data;
+    data = nullptr;
+  }
+  items = cap = 0;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::shrinkToFit() {
+  if (items == cap) return;
+  alloc(items);
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+T Vector<T, INIT_CAP, CAP_MULT>::operator[](int pos) {
+  assert(pos >= 0 && pos < cap && "Position out of bounds!");
+  return data[pos];
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+const T &Vector<T, INIT_CAP, CAP_MULT>::operator[](int pos) const {
+  assert(pos >= 0 && pos < cap && "Position out of bounds!");
+  return data[pos];
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::remove(const T &val) {
+  removeFrom(val);
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::removeAt(int pos) {
+  assert(pos >= 0 && pos < cap && "Position out of bounds!");
+  data[pos] = T();
+  items--;
+  shiftRight(pos); // Effectively moving left.
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::fillDefault(T *ptr, int n) {
+  for (decltype(n) i = 0; i < n; i++) {
+    ptr[i] = T();
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::alloc(int size) {
+  auto newCap = size;
+  auto *newData = new T[newCap];
+  if (newCap > cap) {
+    fillDefault(newData, newCap);
+  }
+  if (items > 0) {
+    memcpy(newData, data, items * sizeof(T));
+    delete[] data;
+  }
+  data = newData;
+  cap = newCap;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::checkAlloc() {
+  if (items != cap) return;
+  alloc(cap == 0 ? INIT_CAP : cap * CAP_MULT);
+}
+
+/// Appends value but only checks capacity if check is set.
+/** This saves processing time when appending very many items. */
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::_append(const T &val, bool check) {
+  if (check) {
+    checkAlloc();
+  }
+  data[items++] = val;
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::shiftRight(int pos) {
+  for (decltype(pos) i = pos; i < items; i++) {
+    std::swap(data[i], data[i+1]);
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::shiftLeft(int pos) {
+  for (decltype(pos) i = items; i >= pos; i--) {
+    std::swap(data[i], data[i+1]);
+  }
+}
+
+template <typename T, int INIT_CAP, int CAP_MULT>
+void Vector<T, INIT_CAP, CAP_MULT>::removeFrom(const T &val, int pos) {
+  for (decltype(pos) i = pos; i < items; i++) {
+    auto &item = data[i];
+    if (item == val) {
+      item = T(); // Clear.
+      items--;
+      shiftRight(i); // Effectively moving left.
+      removeFrom(val, i);
+      break;
+    }
+  }
+}
